a1fa8f52120cc94e03636d8e81f9a0f2bab1de96
Davi Braz <davibraz.profissional@gmail.com>
2024-08-27 12:17:15 -0300

    Salvando a imagem através da API

--- src/main/java/com/example/galeria/application/images/ImageController.java
+++ src/main/java/com/example/galeria/application/images/ImageController.java
@@ -1,8 +1,13 @@
 package com.example.galeria.application.images;
 
+import com.example.galeria.domain.entity.Image;
+import com.example.galeria.domain.enums.ImageExtension;
+import com.example.galeria.domain.service.ImageService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,25 +15,38 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/images")
 @Slf4j
+@RequiredArgsConstructor
 public class ImageController {
 
-    private static final Logger log = LoggerFactory.getLogger(ImageController.class);
+   private final ImageService imageService;
 
     @PostMapping
     public ResponseEntity save(
             @RequestParam("file")MultipartFile file,
             @RequestParam("name") String name,
-            @RequestParam ("tags")List<String> tags) {
+            @RequestParam ("tags")List<String> tags) throws IOException {
 
-        log.info("------ IMAGEM RECEBIDA 9------ ");
+        log.info("------ IMAGEM RECEBIDA ------ ");
         log.info("name: {}, size: {}", file.getOriginalFilename(), file.getSize());
-        log.info("Nome definido para imagem: {}", name);
-        log.info("Tags: {}", tags);
+
+
+
+
+        Image image = Image.builder().name(name)
+                .tags(String.join(",", tags))
+                .size(file.getSize())
+                .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
+                .file(file.getBytes())
+                .build();
+
+        imageService.save(image);
+
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/example/galeria/domain/enums/ImageExtension.java
+++ src/main/java/com/example/galeria/domain/enums/ImageExtension.java
@@ -1,9 +1,23 @@
 package com.example.galeria.domain.enums;
 
-public enum ImageExtension {
-    PNG,
-    JPG,
-    GIF,
-    JPEG,
+import org.springframework.http.MediaType;
 
+import java.util.Arrays;
+
+public enum ImageExtension {
+    PNG(MediaType.IMAGE_PNG),
+    GIF(MediaType.IMAGE_GIF),
+    JPEG(MediaType.IMAGE_JPEG),
+    ;
+
+    private MediaType mediaType;
+
+    ImageExtension(MediaType mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public static ImageExtension valueOf(MediaType mediaType) {
+      return Arrays.stream(values()).filter(ie -> ie.mediaType
+              .equals(mediaType)).findFirst().orElse(null);
+    }
 }
