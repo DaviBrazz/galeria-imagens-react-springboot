e0cf710fd50820fdff02d40d6f8f290430d4594f
Davi Braz <davibraz.profissional@gmail.com>
2024-08-27 21:41:30 -0300

    Adicionando capacidade de executar Specifications no repo de imagens

--- src/main/java/com/example/galeria/infra/repository/ImageRepository.java
+++ src/main/java/com/example/galeria/infra/repository/ImageRepository.java
@@ -1,7 +1,18 @@
 package com.example.galeria.infra.repository;
 
 import com.example.galeria.domain.entity.Image;
+import com.example.galeria.domain.enums.ImageExtension;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ImageRepository extends JpaRepository<Image, String> {
+import java.util.List;
+
+public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecificationExecutor<Image> {
+
+    default List<Image> findByExtensionAndNameOrTagsLike(ImageExtension imageExtension, String query) {
+        if (imageExtension != null) {
+            
+        }
+        return findAll();
+    }
 }
