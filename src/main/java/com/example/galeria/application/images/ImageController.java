c54d89f3b833968106a91c90272ce0ce4f85c40e
Davi Braz <davibraz.profissional@gmail.com>
2024-08-31 20:50:26 -0300

    Corrigindo problemas da pesquisa de imagens

--- src/main/java/com/example/galeria/application/images/ImageController.java
+++ src/main/java/com/example/galeria/application/images/ImageController.java
@@ -60,9 +60,9 @@ public class ImageController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ImageDTO>> search(@RequestParam(value = "extension", required = false) String extension,
+    public ResponseEntity<List<ImageDTO>> search(@RequestParam(value = "extension", required = false, defaultValue ="" ) String extension,
                                                  @RequestParam(value = "query", required = false) String query) {
-        var result = imageService.search(ImageExtension.valueOf(extension), query);
+        var result = imageService.search(ImageExtension.ofName(extension), query);
 
         var images = result.stream().map(image -> {
             var url = buildImageURL(image);
@@ -74,7 +74,11 @@ public class ImageController {
 
     private URI buildImageURL(Image image) {
         String imagePath = "/" + image.getId();
-        return ServletUriComponentsBuilder.fromCurrentRequest().path(imagePath).build().toUri();
+        return ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path(imagePath)
+                .build()
+                .toUri();
     }
 
 
--- src/main/java/com/example/galeria/domain/enums/ImageExtension.java
+++ src/main/java/com/example/galeria/domain/enums/ImageExtension.java
@@ -22,4 +22,11 @@ public enum ImageExtension {
       return Arrays.stream(values()).filter(ie -> ie.mediaType
               .equals(mediaType)).findFirst().orElse(null);
     }
+
+    public static ImageExtension ofName(String name) {
+        return Arrays.stream(values())
+                .filter(ie -> ie.name().equalsIgnoreCase(name))
+                .findFirst()
+                .orElse(null);
+    }
 }
