84450bc13ea8d5d0d75a4fa97873a84b61163244
Davi Braz <davibraz.profissional@gmail.com>
2024-09-02 19:03:35 -0300

    Trazendo as imagens através do front-end

--- galeria/src/app/galeria/page.tsx
+++ galeria/src/app/galeria/page.tsx
@@ -1,13 +1,31 @@
+'use client'
+
 import { Template, ImageCard } from '@/components'
+import { useState } from 'react'
+import { useImageService } from '@/resources/image/image.service'
+import { Image } from '@/resources/image/image.resource';
+
 
 
 export default function GaleriaPage() {
+
+    const useService = useImageService();
+    const [images, setImages] = useState<Image[]>([]);
+
+
+    async function searchImages() {
+        const result = await useService.buscar();
+        setImages(result);
+        console.table(result);
+
+    }
     return (
         <Template>
+            <button className="bg-gray-500" onClick={searchImages}>Clique para mudar</button>
             <section className="grid grid-cols-3 gap-8">
-                <ImageCard nome='Paraiso' tamanho='10MB' dataUpload='01/01/2024' src='https://media.istockphoto.com/id/517188688/pt/foto/paisagem-de-montanha.jpg?s=612x612&w=0&k=20&c=uFGUrUT6gA8FrTWhE10YYzngWPlDLssKxJiDs1Qw2Qs='/>
-                <ImageCard nome='Paraiso' tamanho='10MB' dataUpload='01/01/2024' src='https://media.istockphoto.com/id/517188688/pt/foto/paisagem-de-montanha.jpg?s=612x612&w=0&k=20&c=uFGUrUT6gA8FrTWhE10YYzngWPlDLssKxJiDs1Qw2Qs='/>
-                <ImageCard nome='Paraiso' tamanho='10MB' dataUpload='01/01/2024' src='https://media.istockphoto.com/id/517188688/pt/foto/paisagem-de-montanha.jpg?s=612x612&w=0&k=20&c=uFGUrUT6gA8FrTWhE10YYzngWPlDLssKxJiDs1Qw2Qs='/>
+
+                <ImageCard nome="{nomeImage}" tamanho='10MB' dataUpload='01/01/2024' src="" />
+
             </section>
         </Template>
     )
--- galeriaAPI/src/main/java/com/example/galeria/application/images/ImageController.java
+++ galeriaAPI/src/main/java/com/example/galeria/application/images/ImageController.java
@@ -24,6 +24,7 @@ import java.util.stream.Collectors;
 @RequestMapping("/images")
 @Slf4j
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ImageController {
 
    private final ImageService imageService;
