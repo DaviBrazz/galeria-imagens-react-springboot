a77a3fb0f7e98daeb4da3d3fad2f1a89dd647d76
Davi Braz <davibraz.profissional@gmail.com>
2024-09-02 13:37:51 -0300

    Componente ImageCard e Página galeria

--- galeria/src/app/galeria/page.tsx
+++ galeria/src/app/galeria/page.tsx
@@ -1,9 +1,14 @@
-import { Template } from '../../components/Template'
+import { Template, ImageCard } from '@/components'
+
 
 export default function GaleriaPage() {
     return (
         <Template>
-            <h1>Galeria</h1>
+            <section className="grid grid-cols-3 gap-8">
+                <ImageCard nome='Paraiso' tamanho='10MB' dataUpload='01/01/2024' src='https://media.istockphoto.com/id/517188688/pt/foto/paisagem-de-montanha.jpg?s=612x612&w=0&k=20&c=uFGUrUT6gA8FrTWhE10YYzngWPlDLssKxJiDs1Qw2Qs='/>
+                <ImageCard nome='Paraiso' tamanho='10MB' dataUpload='01/01/2024' src='https://media.istockphoto.com/id/517188688/pt/foto/paisagem-de-montanha.jpg?s=612x612&w=0&k=20&c=uFGUrUT6gA8FrTWhE10YYzngWPlDLssKxJiDs1Qw2Qs='/>
+                <ImageCard nome='Paraiso' tamanho='10MB' dataUpload='01/01/2024' src='https://media.istockphoto.com/id/517188688/pt/foto/paisagem-de-montanha.jpg?s=612x612&w=0&k=20&c=uFGUrUT6gA8FrTWhE10YYzngWPlDLssKxJiDs1Qw2Qs='/>
+            </section>
         </Template>
     )
 }
