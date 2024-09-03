33ff0c1d9e7e58a078bd37c268f8a5c155c9dffc
Davi Braz <davibraz.profissional@gmail.com>
2024-09-03 10:42:49 -0300

    Renderizando as imagens na galeria

--- galeria/src/app/galeria/page.tsx
+++ galeria/src/app/galeria/page.tsx
@@ -19,12 +19,27 @@ export default function GaleriaPage() {
         console.table(result);
 
     }
+
+    function renderImageCard(image: Image) {
+        return (
+            <ImageCard nome={image.name}
+                src={image.url}
+                tamanho={image.size}
+                dataUpload={image.uploadDate} />
+        )
+    }
+
+    function renderImageCards() {
+        return images.map(renderImageCard)
+    }
+
     return (
         <Template>
-            <button className="bg-gray-500" onClick={searchImages}>Clique para mudar</button>
+            <button className="bg-gray-500" onClick={searchImages}>Exibir imagens</button>
             <section className="grid grid-cols-3 gap-8">
-
-                <ImageCard nome="{nomeImage}" tamanho='10MB' dataUpload='01/01/2024' src="" />
+                {
+                    renderImageCards()
+                }
 
             </section>
         </Template>
