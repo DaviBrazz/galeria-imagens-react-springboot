890d9dcd758a156093b516b1232459048d32da42
Davi Braz <davibraz.profissional@gmail.com>
2024-09-03 11:35:21 -0300

    Criação do formulário para pesquissa de imagens

--- galeria/src/app/galeria/page.tsx
+++ galeria/src/app/galeria/page.tsx
@@ -17,7 +17,6 @@ export default function GaleriaPage() {
         const result = await useService.buscar();
         setImages(result);
         console.table(result);
-
     }
 
     function renderImageCard(image: Image) {
@@ -35,7 +34,19 @@ export default function GaleriaPage() {
 
     return (
         <Template>
-            <button className="bg-gray-500" onClick={searchImages}>Exibir imagens</button>
+            <section className="flex flex-col items-center justify-center my-5">
+                <div className="flex space-x-4">
+                    <input type="text" className="border px-3 py-2 rounded-md text-gray-900" />
+                    <select className="border px-4 py-2 rounded-md text-gray-900">
+                        <option >Todos os formatos</option>
+
+                    </select>
+                    <button className="bg-blue-500 text-white px-4 p-2 rounded-lg" onClick={searchImages}>Buscar</button>
+                    <button className="bg-yellow-500 text-white px-4 p-2 rounded-lg">Adicionar nova</button>
+
+                </div>
+
+            </section>
             <section className="grid grid-cols-3 gap-8">
                 {
                     renderImageCards()
